e36823ac825767fffa75ab9e5a0167ca19d0c870
grp <grptestdev@gmail.com>
2020-03-21 12:29:12 -0400

    init add

---
+++ lastboot.ps1
@@ -0,0 +1,6 @@
+﻿if (Test-Connection -ComputerName localhost -Quiet -Count 1 -ErrorAction SilentlyContinue)
+    {
+
+        Get-WmiObject -Class Win32_OperatingSystem -ComputerName localhost | select csname, @{LABEL='LastBootUpTime';EXPRESSION={$_.ConverttoDateTime($_.lastbootuptime)}}
+
+    }
